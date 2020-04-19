ff2c1a431a01ef615388423573bf56b98f5c2df4
le.van.manh <le.van.manh@mqsolutions.com.vn>
2020-04-19 23:02:54 +0700

    update .env

--- src/config/env/auth.js
+++ src/config/env/auth.js
@@ -1,7 +1,7 @@
 const {SECRET_KEY} = process.env;
 
 module.exports = {
-  SECRET_KEY: SECRET_KEY || '2wsjLSopTjD6WQEztTYIZgCFou8wpLJn',
+  SECRET_KEY: SECRET_KEY || 'SQ1FJeQFEMFvtHOaKFKPouCb1DuzS8yY',
   APP_KEY: {
     "4012": "digital",
     "4014": "advertise",
